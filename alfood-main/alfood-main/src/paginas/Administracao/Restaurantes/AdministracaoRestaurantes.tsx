bf5b0c5e2458ec9ab8e20ced8d242a883686ef9d
Adamor Henner <adacandido9@gmail.com>
2023-07-25 14:38:43 -0300

    Editando restaurantes

--- alfood-main/alfood-main/src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
+++ alfood-main/alfood-main/src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
@@ -2,6 +2,7 @@ import { Paper,Table,TableBody,TableCell,TableContainer, TableHead, TableRow } f
 import IRestaurante from "../../../interfaces/IRestaurante";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const AdministracaoRestaurantes = () => {
     const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
@@ -19,12 +20,18 @@ const AdministracaoRestaurantes = () => {
                         <TableCell>
                             Nome
                         </TableCell>
+                        <TableCell>
+                            Editar
+                        </TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     {restaurantes.map(restaurante => <TableRow key={restaurante.id}>
                         <TableCell>
                             {restaurante.nome}
+                        </TableCell>
+                        <TableCell>
+                            [ <Link to={`/admin/restaurantes/${restaurante.id}`}>editar</Link>]
                         </TableCell>
                     </TableRow>)}
                 </TableBody>
--- alfood-main/alfood-main/src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
+++ alfood-main/alfood-main/src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
@@ -1,19 +1,35 @@
 import { Button, TextField } from "@mui/material";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const FormularioRestaurante = () => {
+
+    const parametros = useParams()
+
+    useEffect(() => {
+        if(parametros.id){
+            axios.get(`http://localhost:8000/api/v2/restaurantes/${parametros.id}/`)
+                .then(resposta => setNomeRestaurante(resposta.data.nome))
+        }
+    },[parametros])
 
     const [nomeRestaurante, setNomeRestaurante] = useState('');
 
 
     const aoSubmeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
         evento.preventDefault();
-        axios.post('http://localhost:8000/api/v2/restaurantes/', {nome: nomeRestaurante})
+        if (parametros.id) {
+            axios.put(`http://localhost:8000/api/v2/restaurantes/${parametros.id}/`, {nome: nomeRestaurante})
+            .then(()=> {
+                alert('Restaurante Atualizado com sucesso!');
+            })
+        } else {
+            axios.post('http://localhost:8000/api/v2/restaurantes/', {nome: nomeRestaurante})
             .then(()=> {
                 alert('Restaurante cadastrado com sucesso!');
             })
-
+        }
     }
 
 
