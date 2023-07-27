86a0f5b893bc5c908b02975849e17f3f159c7419
Adamor Henner <adacandido9@gmail.com>
2023-07-26 21:02:28 -0300

    Melhorando layout FormularioRestaurante

--- alfood-main/alfood-main/src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
+++ alfood-main/alfood-main/src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
@@ -1,4 +1,4 @@
-import { Button, TextField } from "@mui/material";
+import { Box, Button, TextField, Typography } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
@@ -34,13 +34,21 @@ const FormularioRestaurante = () => {
 
 
 
-    return (<form onSubmit={aoSubmeterForm}>
-            <TextField value={nomeRestaurante} 
-            onChange={(evento) => setNomeRestaurante(evento.target.value)}
-                label="Nome do Restaurante" 
-                variant="standard" />
-            <Button type="submit" variant="outlined">Salvar</Button>
-        </form>
+    return (
+        <Box sx={{ display:'flex', flexDirection:"column", alignItems:"center"}}>
+            <Typography component="h1" variant="h6">Formulario de Restaurantes</Typography>
+            <Box component="form" onSubmit={aoSubmeterForm}>
+                <TextField 
+                    value={nomeRestaurante}
+                    onChange={(evento) => setNomeRestaurante(evento.target.value)}
+                    label="Nome do Restaurante"
+                    variant="standard" 
+                    fullWidth
+                    required
+                />
+                <Button  sx={{ marginTop: 1 }} type="submit" fullWidth variant="outlined">Salvar</Button>
+            </Box>
+        </Box>
     )
 }
 
